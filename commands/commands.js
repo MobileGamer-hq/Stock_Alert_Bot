17755f098a4d5c015485ffc9184b3edfe5a8f818
mobilegamer-hq <durulego@gmail.com>
2025-05-16 07:05:27 +0100

    maybe

--- commands/commands.js
+++ commands/commands.js
@@ -6,28 +6,29 @@ module.exports = (bot) => {
 <b>📋 Available Commands:</b>
 
 <b>👤 User Commands</b>
-/start – Start your journey with stock alerts<br>
-/help – Learn what this bot does and how to use it<br>
-/about – Get to know your Stock Alert Assistant<br>
-/commands – View all available commands<br>
-/alerts – Get your latest stock alerts<br>
+/start – Start your journey with stock alerts
+/help – Learn what this bot does and how to use it
+/about – Get to know your Stock Alert Assistant
+/commands – View all available commands
+/alerts – Get your latest stock alerts
 
 <b>🛠️ Admin Commands</b>
-/send_message – Send a message to all users<br>
-/send_video – Send a video to all users<br>
-/send_alert – Send a stock alert to all users<br>
-/users – View the number of registered users<br>
-/list_users – List all registered users<br>
-/add_admin &lt;id&gt; – Add a user as an admin<br>
-/remove_admin &lt;id&gt; – Remove a user from admin status<br>
-/list_admins – List all admins<br>
+/send_message – Send a message to all users
+/send_video – Send a video to all users
+/send_alert – Send a stock alert to all users
+/users – View the number of registered users
+/list_users – List all registered users
+/add_admin &lt;id&gt; – Add a user as an admin
+/remove_admin &lt;id&gt; – Remove a user from admin status
+/list_admins – List all admins
 
 <i>For admin commands, make sure you have the necessary permissions.</i>
     `;
 
-    bot.sendMessage(chatId, commandsMessage, { parse_mode: "HTML" })
-      .catch((error) => {
-        console.error('Failed to send /commands message:', error.message);
-      });
+    bot.sendMessage(chatId, commandsMessage, {
+      parse_mode: "HTML",
+    }).catch((error) => {
+      console.error('Failed to send /commands message:', error.message);
+    });
   });
 };
